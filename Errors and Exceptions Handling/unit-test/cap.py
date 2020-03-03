b7775d82b4c9db30d55b6d6fff3c76767337e3e6
MijanurRahman40 <mijanurrahman31416@gmail.com>
2020-03-04 00:52:05 +0600

    data structures added

---
+++ Errors and Exceptions Handling/unit-test/cap.py
@@ -0,0 +1,7 @@
+def capitalText(text):
+    """ 
+    Input a string 
+    Output a capitalized string
+    """
+    # return text.capitalize()
+    return text.title()
--- Errors and Exceptions Handling/unit-test/test_cap.py
+++ Errors and Exceptions Handling/unit-test/test_cap.py
@@ -1,6 +1,20 @@
-def capitalText(text):
-    """ 
-    Input a string 
-    Output a capitalized string
-    """
-    return text.capitalized()
+import unittest
+import cap
+
+# inheritene unittest
+
+
+class TestCap(unittest.TestCase):
+    def testOneWord(self):
+        text = 'python'
+        result = cap.capitalText(text)
+        self.assertEqual(result, 'Python')
+
+    def testMultipleWords(self):
+        text = 'Multi python'
+        result = cap.capitalText(text)
+        self.assertEqual(result, 'Multi python')
+
+
+if __name__ == "__main__":
+    unittest.main()
