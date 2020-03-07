6e6f4b84bfb2c3307463a79fbb437c824d6ca334
MijanurRahman40 <mijanurrahman31416@gmail.com>
2020-03-07 09:48:19 +0600

    alls sre updated

---
+++ advance-python-objects-data-structure/strings.py
@@ -0,0 +1,18 @@
+s = 'hello world'
+
+# capitalize method capitalize the word first letter
+print(s.capitalize())
+
+# upper method capitalize the all letter
+print(s.upper())
+
+print(s.lower())
+
+print(s.find('o'))
+print(s.count('o'))
+
+print(s.center(20, 'z'))
+
+str = 'hello\thi'
+print(str.expandtabs())
+
